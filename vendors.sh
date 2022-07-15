e460235c136901e2ad12cde7b05c2670da5a0459
duplabe <duplabe@users.noreply.github.com>
2022-07-15 17:18:37 +0200

    hugo init

---
+++ vendors.sh
@@ -0,0 +1,8 @@
+#!/bin/zsh
+
+wget https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css -O themes/duplabe/assets/css/bootstrap.min.css
+wget https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css.map -O themes/duplabe/assets/css/bootstrap.min.css.map
+
+wget https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.3.0/css/all.min.css -O themes/duplabe/assets/css/fontawesome-free/css/all.min.css
+wget https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.3.0/webfonts/fa-solid-900.woff2 -O themes/duplabe/assets/css/fontawesome-free/webfonts/fa-solid-900.woff2
+wget https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.3.0/webfonts/fa-solid-900.ttf -O themes/duplabe/assets/css/fontawesome-free/webfonts/fa-solid-900.ttf
